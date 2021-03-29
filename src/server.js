9429648d1cb2aca23c89f709024ceec8663c90ea
Wei Lu <wadelu0714@gmail.com>
2021-03-29 23:24:00 +1300

    Init ejs passport

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 const path = require('path')
 const express = require('express')
-
-const port = process.env.PORT || 5000
+const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
 
+//Static directory
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req, res) => {
-  res.send('Hello express!')
-})
+// EJS
+app.use(expressLayouts)
+app.set('views', path.join(__dirname, '../src/views'))
+app.set('view engine', 'ejs')
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+// Routes
+app.use('/api/passport', require('./routes/index'))
+app.use('/api/passport/users', require('./routes/userRoutes'))
+
+// Port and server start
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
 })
