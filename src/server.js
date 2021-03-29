524b229628cd66d5bfe8097ae4a29bc69232cd8c
Wei Lu <wadelu0714@gmail.com>
2021-03-29 18:59:21 +1300

    prepare the CI/CD on AWS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 
+const port = process.env.PORT || 5000
+
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -10,6 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello express!')
 })
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000')
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
